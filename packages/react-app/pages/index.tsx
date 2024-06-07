e24b81a5d35114f76b1351e38a462adc2869f9b6
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-07 12:06:19 +0100

    added ViewBalance

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
+const { getContract, formatEther, createPublicClient, http } = require("viem");
+const { celo } = require("viem/chains");
+const { stableTokenABI } = require("@celo/abis");
+
+const STABLE_TOKEN_ADDRESS = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 
 export default function Home() {
     const [userAddress, setUserAddress] = useState("");
     const [isMounted, setIsMounted] = useState(false);
     const { address, isConnected } = useAccount();
     const [isMinipay, setIsMinipay] = useState(false);
+    const [balance, setBalance] = useState(0);
 
     useEffect(() => {
         setIsMounted(true);
     }, []);
 
     useEffect(() => {
+        async function fetchBalance() {
+            if (isConnected && address) {
+                let balance = await checkCUSDBalance(publicClient, address);
+                setBalance(balance);
+            }
+        }
+        fetchBalance();
         if (window && window.ethereum) {
             // User has a injected wallet
           
@@ -31,6 +44,29 @@ export default function Home() {
     if (!isMounted) {
         return null;
     }
+    async function checkCUSDBalance(publicClient: any, address: any) {
+        let StableTokenContract = getContract({
+            abi: stableTokenABI,
+            address: STABLE_TOKEN_ADDRESS,
+            publicClient,
+        });
+      
+        let balanceInBigNumber = await StableTokenContract.read.balanceOf([
+            address,
+        ]);
+      
+        let balanceInWei = balanceInBigNumber.toString();
+      
+        let balanceInEthers = formatEther(balanceInWei);
+      
+        return balanceInEthers;
+      }
+      
+      const publicClient = createPublicClient({
+        chain: celo,
+        transport: http(),
+      }); // Mainnet
+      
 
     return (
         <div className="flex flex-col justify-center items-center">
